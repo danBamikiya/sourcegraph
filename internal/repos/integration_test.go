d6498c568f161b9af89718c1788341b28f861199
Molly Weitzel <molly.weitzel@sourcegraph.com>
2021-12-06 13:20:40 -0700

    add test around rate limiting

--- internal/repos/integration_test.go
+++ internal/repos/integration_test.go
@@ -49,6 +49,7 @@ func TestIntegration(t *testing.T) {
 		{"Syncer/ConflictingSyncers", testConflictingSyncers},
 		{"Syncer/SyncRepoMaintainsOtherSources", testSyncRepoMaintainsOtherSources},
 		{"Syncer/SyncReposWithLastErrors", testSyncReposWithLastErrors},
+		{"Syncer/SyncReposWithLastErrorsHitRateLimit", testSyncReposWithLastErrorsHitsRateLimiter},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			db := dbtest.NewFromDSN(t, *dsn)
